0c74603876ba494e145d4a881621b6be303607c5
Baha Taki Eddine <infobaham0780@gmail.com>
2024-11-26 13:27:10 +0100

    Update tp1.c

--- tp1.c
+++ tp1.c
@@ -1,10 +1,9 @@
 
 // ex1
 #include <stdio.h>
-
 int main() 
 {
-  \\1
+//q1
     char Mat[5][5] = {{'1', '2', '3', '4', '5'},{'7', 'a', 'c', '8', 'd'},{'c', '9', '4', 'z', '8'},{'5', '6', 'p', 'n', '3'},{'2', '9', 't', 'm', 'k'}};
     printf("afficher la matrice : \n");
     for(int i = 0; i < 5; i++)
@@ -15,7 +14,7 @@ int main()
         }
         printf("\n");
     }
-\\2
+//q2
     printf("\nLignes pair:\n");
     for(int i = 0; i < 5; i += 2)
     { 
@@ -25,9 +24,9 @@ int main()
         }
         printf("\n");
     }
-    \\3
+   //q3
     printf("\n  Lignes impair :\n");
-    for(int i = 0; i < 5; i++) 
+    for(int i = 0; i < 3; i++) 
     {
         for(int j = 1; j < 5; j += 2) 
         { 
@@ -35,39 +34,60 @@ int main()
         }
         printf("\n");
     }
+//q3
+  // afficher diagonal
+for (int i = 0; i < 3; i++) {
+    printf("%c ", Mat[i][i]);
+}
+printf("\n");
+
+// afficher 2eme diagonal
+for (int i = 0; i < 3; i++) {
+    printf("%c ", Mat[i][4-i]);
+}
+printf("\n");  
     return 0;
 }
 
 \\ ex 2 
-#include<stdio.h>
-int main () 
-{
-int A[4][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-     int T;
-     int i,j;
-     printf("tableau inital:\n");
-     for (i=0;i<4;i++){
-        for(j=0;j<4;j++){
-                printf("%d\t",A[i][j]);
-        }
-        printf("\n");
-     }
-     for (i=0;i<3;i++){
-        for(j=i+1;j<4;j++){
-            T=A[i][j];
-            A[i][j]=A[j][i];
-            A[j][i]=T;
-        }
-     }
-     printf("tableau echange:\n");
-     for (i=0;i<4;i++){
-        for(j=0;j<4;j++){
-                printf("%d\t",A[i][j]);
-        }
-               printf("\n");
-     }
-    return 0;
+#include <stdio.h>
+
+int main() {
+    int i,j,s,t;
+    int m[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+      printf("give the size of your matrix = ");
+      scanf("%d",&s);
+    printf("the first matrix : ");
+    printf("\n");
+    for ( i = 0; i < s; i++) {
+        for (int j = 0; j < s; j++) {
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+    printf("the secod matrix :");
+    printf("\n");
+    for ( i = 0; i < s; i++) {
+        for (j = i + 1; j < s; j++) {
+             t = m[i][j];
+            m[i][j] = m[j][i];
+            m[j][i] = t;
+        }
+    }
+    for ( i = 0; i < s; i++) {
+        for (int j = 0; j < s; j++) {
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
 }
+    
 
     
 
